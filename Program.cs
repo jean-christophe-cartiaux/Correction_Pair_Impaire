2603d38bfd3d004a9d3a883ae964cab8e2da28e6
Jean-Christophe Cartiaux <jc.cartiaux@stag.technofuturtic.education>
2023-09-04 09:54:56 +0200

    Corectif paire impaire

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Correctif_C_Sharp_Base
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nbr;
+            Console.WriteLine("Enter de nomber pliiizzzz !");
+           nbr= Int32.Parse( Console.ReadLine());
+
+        }
+    }
+}
